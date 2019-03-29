0067fd9b6aa9daba2cf50635c122d146e1364ce8
Kevin Smith <keveightysev@gmail.com>
2019-03-29 11:58:38 -0500

    Actions POST working

---
+++ routes/actions.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const Actions = require('../models/actions.js');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+	try {
+		const actions = await Actions.getActions(req.projectId);
+		res.status(200).json(actions);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+});
+
+router.post('/', async (req, res) => {
+	if (!req.body.description) {
+		res
+			.status(406)
+			.json({ message: 'Please enter a description for the action' });
+		return;
+	}
+	const newProject = {
+		...req.body,
+		project_id: req.projectId,
+	};
+	try {
+		const action = await Actions.addAction(newProject);
+		res.status(201).json(action);
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+});
+
+module.exports = router;
--- routes/projects.js
+++ routes/projects.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Projects = require('../models/projects.js');
+const actionsRouter = require('./actions.js');
 
 const router = express.Router();
 
@@ -15,10 +16,12 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
 	if (!req.body.name) {
 		res.status(406).json({ message: 'Please enter a name for the project' });
+		return;
 	} else if (!req.body.description) {
 		res
 			.status(406)
 			.json({ message: 'Please enter a description for the project' });
+		return;
 	}
 	try {
 		const project = await Projects.addProject(req.body);
@@ -27,5 +30,14 @@ router.post('/', async (req, res) => {
 		res.status(500).json(err);
 	}
 });
+
+router.use(
+	'/:projectId/actions',
+	(req, res, next) => {
+		req.projectId = req.params.projectId;
+		next();
+	},
+	actionsRouter,
+);
 
 module.exports = router;
