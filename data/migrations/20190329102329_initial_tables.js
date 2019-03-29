27b8e2dc8c4f92474ea87efe43521e1686522a30
Kevin Smith <keveightysev@gmail.com>
2019-03-29 10:35:55 -0500

    Migrated tables and created DB

---
+++ data/migrations/20190329102329_initial_tables.js
@@ -0,0 +1,57 @@
+exports.up = function(knex) {
+	return knex.schema
+		.createTable('projects', table => {
+			table.increments();
+			table.string('name').notNullable();
+			table.string('description').notNullable();
+			table.boolean('completed');
+		})
+
+		.createTable('actions', table => {
+			table.increments();
+			table.string('description').notNullable();
+			table.string('notes');
+			table.boolean('completed');
+			table
+				.integer('project_id')
+				.unsigned()
+				.notNullable()
+				.references('id')
+				.inTable('projects')
+				.onDelete('CASCADE')
+				.onUpdate('CASCADE');
+		})
+
+		.createTable('contexts', table => {
+			table.increments();
+			table.string('location').notNullable();
+		})
+
+		.createTable('actions_contexts', table => {
+			table.increments();
+			table
+				.integer('action_id')
+				.unsigned()
+				.notNullable()
+				.references('id')
+				.inTable('actions')
+				.onDelete('CASCADE')
+				.onUpdate('CASCADE');
+			table
+				.integer('context_id')
+				.unsigned()
+				.notNullable()
+				.references('id')
+				.inTable('contexts')
+				.onDelete('CASCADE')
+				.onUpdate('CASCADE');
+		});
+};
+
+exports.down = function(knex) {
+	return knex.schema
+		.dropTableIfExists('actions_contexts')
+		.dropTableIfExists('contexts')
+		.dropTableIfExists('actions')
+		.dropTableIfExists('projects');
+};
